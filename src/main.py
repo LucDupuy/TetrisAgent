d2075e54a31afe698820ad7f766b85114de63c7c
LucDupuy <luc.dupuy@ryerson.ca>
2021-02-28 15:40:13 -0500

    Set up the main file a bit, so it makes some sense, just unsure of how to overwrite the built in policy

--- src/main.py
+++ src/main.py
@@ -2,16 +2,43 @@ from nes_py.wrappers import JoypadSpace
 import gym_tetris
 from gym_tetris.actions import MOVEMENT
 
-env = gym_tetris.make('TetrisA-v0')
-env = JoypadSpace(env, MOVEMENT)
 
-done = True
-for step in range(5000):
-    if done:
-        state = env.reset()
 
-    # Think we can just make methods to set these 4 vars ourselves
-    state, reward, done, info = env.step(env.action_space.sample())
+
+
+def policy_iteration():
+    return
+
+
+
+
+
+
+def render_env(policy, max_steps):
+
+    episode_reward = 0
+    board_state = env.reset()
+
+    for step in range(max_steps):
+        env.render()
+
+        a = policy[board_state]
+        board_state, reward, done, info = env.step(a)
+        episode_reward += reward
+        if done:
+            break
+
     env.render()
 
-env.close()
+
+
+
+
+if __name__ == "__main__":
+    env = gym_tetris.make('TetrisA-v0')
+    env = JoypadSpace(env, MOVEMENT)
+
+    pol = policy_iteration()
+
+
+    render_env(pol, 5000)
